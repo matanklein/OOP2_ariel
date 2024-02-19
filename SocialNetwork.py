8c6130c80b2db11151e08fb4682483f3b49eaaa0
matanKlein <matan.Klein1@msmail.ariel.ac.il>
2024-02-19 10:43:09 +0200

    adding SocialNetwork.py

---
+++ SocialNetwork.py
@@ -0,0 +1,7 @@
+class SocialNetwork:
+    __instance = None
+
+    def __new__(cls):
+        if(cls.__instance is None):
+            cls.__instance = super.__new__(cls)
+        return cls.__instance
